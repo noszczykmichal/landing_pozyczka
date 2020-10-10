e278fdc6ea82ab0cd58a60947cafe410b0c19e33
Michał Noszczyk <noszczykmichal@wp.pl>
2020-10-10 16:30:35 +0200

    counters launch bound with scroll event

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 $(function(){
-    $('.counter-count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 5000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
-});
+    $(document).bind('scroll', function(ev) {
+        var scrollOffset = $(document).scrollTop();
+        var containerOffset = $('.counter').offset().top - window.innerHeight;
+        if (scrollOffset > containerOffset) {
+            $('.counter-count').each(function () {
+                $(this).prop('Counter',0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 5000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+            $(document).unbind('scroll');
+        }
+    });   
+})
+
+
+
+
