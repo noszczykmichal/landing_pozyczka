18d9ac9b54166e007e6c717a7c0b5bb53cf3b5a3
Michał Noszczyk <noszczykmichal@wp.pl>
2020-10-13 00:38:16 +0200

    section AboutUs ready

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-$(function(){
-    $(document).bind('scroll', function(ev) {
+$(function () {
+    //launching of the counters bound with a scroll event + code for the counters animation
+
+    $(document).bind('scroll', function (ev) {
         var scrollOffset = $(document).scrollTop();
         var containerOffset = $('.counter').offset().top - window.innerHeight;
         if (scrollOffset > containerOffset) {
             $('.counter-count').each(function () {
-                $(this).prop('Counter',0).animate({
+                $(this).prop('Counter', 0).animate({
                     Counter: $(this).text()
                 }, {
                     duration: 5000,
@@ -16,55 +18,63 @@ $(function(){
             });
             $(document).unbind('scroll');
         }
-    });   
+    });
 
-const settings = {
-	'async': true,
-	'crossDomain': true,
-	'url': 'https://bloomberg-market-and-financial-news.p.rapidapi.com/market/get-cross-currencies?id=chf%252Ceur%252Cgbp%252Cpln%252Cusd',
-	'method': 'GET',
-	'headers': {
-		'x-rapidapi-host': 'bloomberg-market-and-financial-news.p.rapidapi.com',
-		'x-rapidapi-key': 'ba18d7ebc0msh4cd6e446bb748d3p1d8e2djsna317ec0460a1'
-	}
-}
+    //options for the request to the api
 
-$.ajax(settings).done(function (response) {
-    const eur=response.result['eurpln:cur'];
-    const usd=response.result['usdpln:cur'];
-    const chf=response.result['chfpln:cur'];
-    const gbp=response.result['gbppln:cur'];
-    
-    const currencies=[eur,usd,chf,gbp];
-    
+    // const settings = {
+    //     'async': true,
+    //     'crossDomain': true,
+    //     'url': 'https://bloomberg-market-and-financial-news.p.rapidapi.com/market/get-cross-currencies?id=chf%252Ceur%252Cgbp%252Cpln%252Cusd',
+    //     'method': 'GET',
+    //     'headers': {
+    //         'x-rapidapi-host': 'bloomberg-market-and-financial-news.p.rapidapi.com',
+    //         'x-rapidapi-key': 'ba18d7ebc0msh4cd6e446bb748d3p1d8e2djsna317ec0460a1'
+    //     }
+    // }
 
-   $('.quote').each(function(index){
-       $(this).text(currencies[index].last);
-   })
+    // $.ajax(settings).done(function (response) {
+    //     const eur = response.result['eurpln:cur'];
+    //     const usd = response.result['usdpln:cur'];
+    //     const chf = response.result['chfpln:cur'];
+    //     const gbp = response.result['gbppln:cur'];
 
-   $('.date').each(function(index){
-       const date=new Date(currencies[index].lastPriceTime*1000);
-       if(date.getUTCDate()<10){
-           $(this).text('0'+date.getUTCDate()+'-'+(date.getUTCMonth()+1)+'-'+date.getUTCFullYear());
-       }else{
-        $(this).text(date.getUTCDate()+'-'+(date.getUTCMonth()+1)+'-'+date.getUTCFullYear());
-       }
-    
-   })
+    //     //an array created from the response for a given currency
 
-   $('.pctChange').each(function(index){
-       const pctChange=parseFloat(currencies[index].pctChange);
+    //     const currencies = [eur, usd, chf, gbp];
 
-       if(pctChange<0){
-           $(this).css('color', 'red').text(pctChange+'%');
-       }else if(pctChange==0){
-        $(this).css('color', 'black').text(pctChange+'%');
-       }else{
-        $(this).css('color', 'green').text(pctChange+'%');
-       }
-   })
+    //     //setting text for the element with value of the last quotes
 
-});
+    //     $('.quote').each(function (index) {
+    //         $(this).text(currencies[index].last);
+    //     })
+
+    //     //setting the date of the last quote
+    //     $('.date').each(function (index) {
+    //         const date = new Date(currencies[index].lastPriceTime * 1000);
+    //         if (date.getUTCDate() < 10) {
+    //             $(this).text('0' + date.getUTCDate() + '-' + (date.getUTCMonth() + 1));
+    //         } else {
+    //             $(this).text(date.getUTCDate() + '-' + (date.getUTCMonth() + 1));
+    //         }
+
+    //     })
+
+    //     //setting the percentage change for a given currency
+
+    //     $('.pctChange').each(function (index) {
+    //         const pctChange = parseFloat(currencies[index].pctChange);
+
+    //         if (pctChange < 0) {
+    //             $(this).css('color', 'red').text(pctChange + '%');
+    //         } else if (pctChange == 0) {
+    //             $(this).css('color', 'black').text(pctChange + '%');
+    //         } else {
+    //             $(this).css('color', 'green').text(pctChange + '%');
+    //         }
+    //     })
+
+    // });
 
 })
 
